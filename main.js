b88cd3f286f615f273195b9942927da2fcace3bd
Raphael <guet.raphael@hotmail.com>
2022-09-20 16:16:22 +0200

    Affichage des temperatures et des heures

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const loader = document.querySelector(".loader");
-
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     (location) => {
@@ -7,6 +6,7 @@ if (navigator.geolocation) {
       const latitude = location.coords.latitude;
       console.log(location, long, latitude);
       getWeatherData(long, latitude);
+      getForecastWeatherData(long, latitude);
     },
     () => {
       loader.textContent =
@@ -14,19 +14,37 @@ if (navigator.geolocation) {
     }
   );
 }
-
+//Donnée de prévision
+async function getForecastWeatherData(long, latitude) {
+  try {
+    const resultsForecast = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${long}&lang=fr&units=metric&appid=150dcb455a3497c432d9faf1e3e7dcf5`
+    );
+    if (!resultsForecast.ok) {
+      console.log("erreur du forecast");
+    }
+    const dataForecast = await resultsForecast.json();
+    handleHour(dataForecast);
+    console.log(dataForecast);
+  } catch (error) {
+    loader.textContent = error;
+  }
+}
+// Temps actuel
 async function getWeatherData(long, latitude) {
   try {
     const results = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${long}&appid=12f3a215e8274b83bcafb225f7f11e31`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${long}&lang=fr&units=metric&appid=150dcb455a3497c432d9faf1e3e7dcf5`
     );
     if (!results.ok) {
+      console.log(results);
       throw new Error(`Erreur : ${results}`);
     }
     const data = await results.json();
-
     console.log(data);
     populateMainInfo(data);
+
+    console.log(data.weather[0].description);
     loader.classList.add("fade-out");
   } catch (error) {
     loader.textContent = error;
@@ -38,12 +56,32 @@ const temperature = document.querySelector(".temperature");
 const weatherImage = document.querySelector(".weather-logo");
 const currentHour = new Date().getHours();
 function populateMainInfo(data) {
-  temperature.textContent = `${Math.trunc(data.weather.temp)}`;
-  position.textContent = data.timezone;
+  temperature.textContent = `${Math.trunc(data.main.temp)}°`;
+  position.textContent = data.name;
 
   if (currentHour >= 6 && currentHour < 21) {
     weatherImage.src = `ressources/jour/${data.weather[0].icon}.svg`;
   } else {
     weatherImage.src = `ressources/nuit/${data.weather[0].icon}.svg`;
   }
+}
+
+const hourNameBlocks = document.querySelectorAll(".hour-name");
+const hourTempartures = document.querySelectorAll(".hour-temp");
+function handleHour(dataForecast) {
+  hourNameBlocks.forEach((block, index) => {
+    const incrementedHour = currentHour + index * 3;
+    if (incrementedHour > 24) {
+      const calcul = incrementedHour - 24;
+      hourNameBlocks[index].textContent = `${calcul === 24 ? "00" : calcul}h`;
+    } else if (incrementedHour === 24) {
+      hourNameBlocks[index].textContent = "00h";
+    } else {
+      hourNameBlocks[index].textContent = `${incrementedHour}h`;
+    }
+    hourTempartures[index].textContent = `${Math.trunc(
+      dataForecast.list[index].main.temp
+    )}°`;
+    console.log(Math.trunc(dataForecast.list[index * 3].main.temp));
+  });
 }
